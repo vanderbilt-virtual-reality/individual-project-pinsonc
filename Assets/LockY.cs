2acb851c9fabe4a20586966ad57a8a6022f40c0f
Conner Pinson <pinsonconner@gmail.com>
2019-11-21 23:46:22 -0600

    finished out the theater, added grabbables

---
+++ Assets/LockY.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockY : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //This locks the RigidBody so that it does not move or rotate in the Z axis.
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+    }
+}
